b7bade4b4ca35c6e9f0cd0c63100671660a02c58
Fraa-00 <francescogaza007@gmail.com>
2025-06-11 09:48:46 +0200

    Update Training.py

--- Training.py
+++ Training.py
@@ -59,6 +59,8 @@ def train_loop(
             if second_encoder:
                 feat2 = second_encoder(imgs)
                 feat2_flat = feat2.flatten(2).permute(0, 2, 1)
+                print(feat2_flat.shape, feat2.shape)  # Debugging line to check shapes
+                feat2_flat = torch.max(feat2_flat, dim=1)[0]  # (B, C)
                 feats = torch.cat([feat1_flat, feat2_flat], dim=1)
             else:
                 feats = feat1_flat
